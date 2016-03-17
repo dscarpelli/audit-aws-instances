eb57c13a335d6ca381162a3cee88bef7a5f9685c
dscarpelli <dscarpelli@users.noreply.github.com>
2016-03-17 19:00:22 -0400

    update for use with aws cli

--- audit-aws-instances.sh
+++ audit-aws-instances.sh
@@ -8,8 +8,12 @@
 
 used_types="m3.medium m3.large m3.xlarge m3.2xlarge r3.large r3.xlarge r3.2xlarge r3.4xlarge r3.8xlarge"
 
-active_res=$(ec2-describe-reserved-instances --filter state=active)
-active_ins=$(ec2-describe-instances --filter instance-state-name=running)
+source ~/.profile
+
+active_res=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active --output text)
+active_ins=$(/usr/local/bin/aws ec2 describe-instances --filter Name=instance-state-name,Values=running --output text)
+
+exit_code=0
 
 # Loop through reserved instances
 
@@ -17,17 +21,19 @@ for type in $used_types; do
   res_total=0
   ins_total=0
   while read -r instance; do
-    qty=$(echo "$instance" | awk '{print $11}')
+    qty=$(echo "$instance" | awk '{print $7}')
     res_total=$(expr "$res_total" + "$qty")
   done < <(echo "$active_res" | grep "$type")
   ins_total=$(echo "$active_ins" | grep -c "$type")
   diff=$(($res_total - $ins_total))
   if [[ $diff -gt 0 ]]; then
     echo "UNUSED RESERVATION: (${diff}) - $type"
+    exit_code=1
   elif [[ $diff -lt 0 ]]; then
     diff=$(echo $diff | sed 's/-//g')
     echo "UNRESERVED INSTANCES: (${diff}) - $type"
-  else
-    echo "OK - $type"
+    exit_code=1
   fi
 done
+
+exit $exit_code
