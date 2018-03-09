f92f76d3ca00de7d96000ba1d06f34b56029377d
dscarpelli <dscarpelli@users.noreply.github.com>
2018-03-09 09:28:04 -0500

    Support convertible RIs, add crit value, add m4s
    Add support for convertible reserved instances.  Script now returns exit code 2 (critical) if any RI type exceeds $crit outstanding.  Added a few common m4 instance types.

--- audit-aws-instances.sh
+++ audit-aws-instances.sh
@@ -6,11 +6,15 @@
 # corresponding reserved instances.
 ###########################################
 
-used_types="m3.medium m3.large m3.xlarge m3.2xlarge r3.large r3.xlarge r3.2xlarge r3.4xlarge r3.8xlarge"
+# Critical (exit code 2) if > this number
+crit=4
+
+used_types="m3.medium m3.large m3.xlarge m3.2xlarge m4.large m4.xlarge m4.2xlarge m4.4xlarge r3.large r3.xlarge r3.2xlarge r3.4xlarge r3.8xlarge"
 
 source ~/.profile
 
-active_res=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active --output text)
+active_res=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active Name=scope,Values="Availability Zone" --output text)
+active_conv=$(/usr/local/bin/aws ec2 describe-reserved-instances --filter Name=state,Values=active Name=scope,Values=Region --output text)
 active_ins=$(/usr/local/bin/aws ec2 describe-instances --filter Name=instance-state-name,Values=running --output text)
 
 exit_code=0
@@ -24,15 +28,30 @@ for type in $used_types; do
     qty=$(echo "$instance" | awk '{print $7}')
     res_total=$(expr "$res_total" + "$qty")
   done < <(echo "$active_res" | grep "$type")
+  while read -r instance; do
+    qty=$(echo "$instance" | awk '{print $6}')
+    res_total=$(expr "$res_total" + "$qty")
+  done < <(echo "$active_conv" | grep "$type")
   ins_total=$(echo "$active_ins" | grep -c "$type")
   diff=$(($res_total - $ins_total))
-  if [[ $diff -gt 0 ]]; then
+  if [[ $diff -gt $crit ]]; then
     echo "UNUSED RESERVATION: (${diff}) - $type"
-    exit_code=1
+    exit_code=2
+  elif [[ $diff -gt 0 ]]; then
+    echo "UNUSED RESERVATION: (${diff}) - $type"
+    if [[ $exit_code -ne 2 ]]; then
+      exit_code=1
+    fi
+  elif [[ $diff -lt -${crit} ]]; then
+    diff=$(echo $diff | sed 's/-//g')
+    echo "UNRESERVED INSTANCES: (${diff}) - $type"
+    exit_code=2
   elif [[ $diff -lt 0 ]]; then
     diff=$(echo $diff | sed 's/-//g')
     echo "UNRESERVED INSTANCES: (${diff}) - $type"
-    exit_code=1
+    if [[ $exit_code -ne 2 ]]; then
+      exit_code=1
+    fi
   fi
 done
 
